951c7b246af0f8bbd1e70a115ba9532580e9303e
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2023-11-28 17:14:28 -0500

    hello world dash

--- new.py
+++ new.py
@@ -1,1 +1,11 @@
-print ('hello world')
+import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(children='Hello World')
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
