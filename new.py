b07f9daa1b2d7e748e742d1c3ffbdae04d4e6183
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2023-11-29 15:36:20 -0500

    define columns , add to data table

--- new.py
+++ new.py
@@ -6,24 +6,27 @@ app = Dash(__name__)
 
 # Fetching data from the API
 try:
-    url = "https://api.football-data.org/v4/matches"
+    url = "https://www.balldontlie.io/api/v1/players"
     # headers = {
     #     "X-RapidAPI-Key": "92553ecf58msh5e3bfb1d615a421p1bd6cejsne10eac083a7c",
     #     "X-RapidAPI-Host": "americanfootballapi.p.rapidapi.com"
     # }
     response = requests.get(url)
     data = response.json()
-    df = pd.DataFrame(data)  # Convert JSON data to DataFrame
+    df = pd.DataFrame(data['data'][0])  # Convert JSON data to DataFrame
     print(df.head())
 except Exception as e:
     print("Error fetching data:", e)
     df = pd.DataFrame()  # Creating an empty DataFrame if data fetch fails
 
+# Define columns
+columns = [{'name': i, 'id': i} for i in df.columns]
+
 #construct the layout of the app
 app.layout = html.Div([
-    html.Div(children='Fantasy Football'),
+    html.Div(children='NBA Player Data'),
     dash_table.DataTable(
-        data=df.to_dict('records')
+        data=df.to_dict('records'), columns=columns
         )
 ])
 
