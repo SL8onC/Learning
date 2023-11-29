e049feec0a1611aafdeb2728d2704d756e7614e7
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2023-11-29 15:21:41 -0500

    add correct api key. try to print table

--- new.py
+++ new.py
@@ -6,7 +6,7 @@ app = Dash(__name__)
 
 # Fetching data from the API
 try:
-    url = "https://americanfootballapi.p.rapidapi.com/api/american-football/search/brady"
+    url = "https://americanfootballapi.p.rapidapi.com/api/american-football/search/mahomes"
     headers = {
         "X-RapidAPI-Key": "92553ecf58msh5e3bfb1d615a421p1bd6cejsne10eac083a7c",
         "X-RapidAPI-Host": "americanfootballapi.p.rapidapi.com"
@@ -14,16 +14,19 @@ try:
     response = requests.get(url, headers=headers)
     data = response.json()
     df = pd.DataFrame(data)  # Convert JSON data to DataFrame
-    df.set_index('column_name', inplace=True)
     print(df.head())
 except Exception as e:
     print("Error fetching data:", e)
     df = pd.DataFrame()  # Creating an empty DataFrame if data fetch fails
 
+#construct the layout of the app
 app.layout = html.Div([
     html.Div(children='Fantasy Football'),
-    dash_table.DataTable(data=df.to_dict('records'))
+    dash_table.DataTable(
+        data=df.to_dict('records')
+        )
 ])
 
+#run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
