9d2c2bd5feb5b581dc6c7eb26120e1adb4aa6469
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2023-11-29 19:30:42 -0500

    define dropdown

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc, Output, Input
 import pandas as pd
 import requests
 
@@ -7,28 +7,29 @@ app = Dash(__name__)
 # Fetching data from the API
 try:
     url = "https://www.balldontlie.io/api/v1/players"
-    # headers = {
-    #     "X-RapidAPI-Key": "92553ecf58msh5e3bfb1d615a421p1bd6cejsne10eac083a7c",
-    #     "X-RapidAPI-Host": "americanfootballapi.p.rapidapi.com"
-    # }
-    response = requests.get(url)
-    data = response.json()
-    df = pd.DataFrame(data['data'][0])  # Convert JSON data to DataFrame
-    print(df.head())
+    data = requests.get(url).json() # get the response from the APi endpoint and turn it into a JSON
+    players_df = pd.DataFrame(data['data'])  # Convert JSON data to DataFrame
 except Exception as e:
     print("Error fetching data:", e)
     df = pd.DataFrame()  # Creating an empty DataFrame if data fetch fails
 
-# Define columns
-columns = [{'name': i, 'id': i} for i in df.columns]
 
 #construct the layout of the app
 app.layout = html.Div([
     html.Div(children='NBA Player Data'),
-    dash_table.DataTable(
-        data=df.to_dict('records'), columns=columns
-        )
+    dcc.Dropdown(
+        id = 'player-dropdown',
+        options=[{'label': player['first_name'] + ' ' + player['last_name'] +'  |  ' + player['position'], 'value':idx } for idx, player in players_df.iterrows()],
+        value=237
+    ),
+
+    dash_table.DataTable(id = 'player-table')
 ])
+
+# Callbacks
+@callback(
+    Output
+)
 
 #run the app
 if __name__ == '__main__':
